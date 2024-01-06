80efbe413888d64de78810f7ac4a9e3f963711a2
Reinout Schols <reinoutschols@hotmail.com>
2024-01-06 17:40:19 +0100

    added ESlint to my project

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express')
 const morgan = require('morgan')
 const app = express()
 const cors = require('cors')
-const mongoose = require('mongoose')
 require('dotenv').config()
 const Contact = require('./models/contact')
 
@@ -10,42 +9,42 @@ app.use(express.static('dist'))
 app.use(express.json())
 app.use(cors())
 
-
-// Using morgan to simplify logging 
+// Using morgan to simplify logging
 morgan.token('type', (req, res) => {
   return JSON.stringify(req.body)
-});
+})
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :type'))
 
 // Getting all contacts
 app.get('/api/persons', (request, response, next) => {
   Contact.find({}).then(contacts => {
-    if(contacts) {
+    if (contacts) {
       response.json(contacts)
     } else {
-      throw {status: 404, message: "No contacts found."}
+      // eslint-disable-next-line no-throw-literal
+      throw { status: 404, message: 'No contacts found.' }
     }
   })
-  .catch(error => next(error))
-  })
-
+    .catch(error => next(error))
+})
 
 //  getting the info of the phonebook
 app.get('/info', (request, response) => {
-     Contact.countDocuments({})
-     .then((numberOfEntries) => {
+  Contact.countDocuments({})
+    .then((numberOfEntries) => {
       console.log(numberOfEntries)
-      const dateRequest =  new Date().toUTCString();
+      const dateRequest = new Date().toUTCString()
       response.send(`<p>Phonebook has info for ${numberOfEntries} people</p> ${dateRequest}`)
     })
+    // eslint-disable-next-line no-undef
     .catch((error) => next(error))
 })
 
 // Getting contacts by ID
-  app.get('/api/persons/:id', (request, response, next) => {
-    const id = request.params.id;
-    console.log('Fetching person with ID:', id);
-    Contact.findById(id)
+app.get('/api/persons/:id', (request, response, next) => {
+  const id = request.params.id
+  console.log('Fetching person with ID:', id)
+  Contact.findById(id)
     .then(result => {
       if (result) {
         response.json(result)
@@ -54,59 +53,59 @@ app.get('/info', (request, response) => {
       }
     })
     .catch(error => next(error))
-  })
+})
 
-  // Deleting contacts
+// Deleting contacts
 app.delete('/api/persons/:id', (request, response, next) => {
-    Contact.findByIdAndDelete(request.params.id)
+  Contact.findByIdAndDelete(request.params.id)
     .then(result => {
       if (result) {
         response.status(204).end()
-    } else {
-      throw {status: 404, message: "Contact not found"}
+      } else {
+        // eslint-disable-next-line no-throw-literal
+        throw { status: 404, message: 'Contact not found' }
       }
     })
     .catch(error => next(error))
-  })
+})
 
-   // Creating new contacts
+// Creating new contacts
 app.post('/api/persons', (request, response, next) => {
-  const body = request.body;
-  const contact = new Contact ({ 
-       name: body.name,
-      number: body.number
-      });
+  const body = request.body
+  const contact = new Contact({
+    name: body.name,
+    number: body.number
+  })
   contact.save().then(savedContact => {
-    console.log('Contact saved successfully:', savedContact);
+    console.log('Contact saved successfully:', savedContact)
     response.json(savedContact)
   })
-  .catch(error => next(error))
+    .catch(error => next(error))
 })
 
 // updating contacts
 app.put('/api/persons/:id', (request, response, next) => {
-  const body = request.body;
+  const body = request.body
 
   Contact.findOne({ name: body.name })
     .then(existingContact => {
       if (existingContact) {
-        existingContact.number = body.number;
-        return existingContact.save();
+        existingContact.number = body.number
+        return existingContact.save()
       } else {
         const newContact = new Contact({
           name: body.name,
           number: body.number
-        });
-        return newContact.save();
+        })
+        return newContact.save()
       }
     })
     .then(updatedContact => {
-      console.log('Contact updated/saved successfully:', updatedContact);
-      response.json(updatedContact);
+      console.log('Contact updated/saved successfully:', updatedContact)
+      response.json(updatedContact)
     })
     .catch(error => next(error))
-  })
-
+})
 
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
@@ -115,22 +114,20 @@ const unknownEndpoint = (request, response) => {
 app.use(unknownEndpoint)
 
 const errorHandler = (error, request, response, next) => {
-  console.log(error.message);
+  console.log(error.message)
 
-  if (error.name === "CastError") {
-    return response.status(400).send({ error: 'malformatted id' });
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
   } else if (error.name === 'ValidationError') {
-    //console.log('Validation error:', error.message);
-    return response.status(400).json({ error: error.message });
-  } 
-  next(error);
-};
+    // console.log('Validation error:', error.message)
+    return response.status(400).json({ error: error.message })
+  }
+  next(error)
+}
 
 app.use(errorHandler)
 
-
-
-const PORT = process.env.PORT 
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
--- models/contact.js
+++ models/contact.js
@@ -1,42 +1,42 @@
-const mongoose = require('mongoose');
-require('dotenv').config();
+const mongoose = require('mongoose')
+require('dotenv').config()
 
-const url = process.env.MONGODB_URI;
+const url = process.env.MONGODB_URI
 
-console.log('Connecting to MongoDB...', url);
+console.log('Connecting to MongoDB...', url)
 mongoose.connect(url)
-    .then(result => {
-        console.log('Connected to MongoDB');
-    })
-    .catch((error) => {
-        console.log('Error connecting to MongoDB:', error.message);
-    });
+  .then(result => {
+    console.log('Connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('Error connecting to MongoDB:', error.message)
+  })
 
 const phonebookSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        minLength: 3, 
-        required: true
-    },
-    number: {
-        type: String,
-        minLength: 8,
-        required: true, 
+  name: {
+    type: String,
+    minLength: 3,
+    required: true
+  },
+  number: {
+    type: String,
+    minLength: 8,
+    required: true,
     validate: {
-        validator: function(v) {
-            return /\d{2}-\d{8}/.test(v) || /\d{3}-\d{7}/.test(v)
-          }, 
-          message: props => `${props.value} is not a valid phone number!`
-        }}
-    });
-
+      validator: function (v) {
+        return /\d{2}-\d{8}/.test(v) || /\d{3}-\d{7}/.test(v)
+      },
+      message: props => `${props.value} is not a valid phone number!`
+    }
+  }
+})
 
 phonebookSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-      returnedObject.id = returnedObject._id.toString()
-      delete returnedObject._id
-      delete returnedObject.__v
-    }
-  })
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
 
 module.exports = mongoose.model('Contact', phonebookSchema)
